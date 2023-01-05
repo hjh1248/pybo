ab420debc7be1f98c2058bde81cbe0a831cfc242
hjh1248 <jh980509@gmail.com>
2023-01-06 08:42:31 +0900

    날짜, 시간 변경

--- pybo/serializer.py
+++ pybo/serializer.py
@@ -5,10 +5,4 @@ from .models import Question
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ['author', 'subject', 'content', 'create_date', 'modify_date', 'voter']
-    author = serializers.CharField(max_length=100)
-    subject = serializers.CharField(max_length=100)
-    content = serializers.CharField(max_length=100)
-    created_date = serializers.DateTimeField(input_formats=["%Y-%m-%d"])
-    created_date = serializers.DateTimeField(input_formats=["%Y-%m-%d"])
-    voter = serializers.CharField(max_length=100)
+        fields = '__all__'
